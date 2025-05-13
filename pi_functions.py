bf617ea3d4f52fca0ad65c34b666b153be43dda3
zenmodeman <123277820+zenmodeman@users.noreply.github.com>
2025-05-13 13:31:21 +0000

    Fix the number of pi digits being acquired

--- pi_functions.py
+++ pi_functions.py
@@ -3,7 +3,7 @@ import random
 
 NUM_MONS = 1025
 
-def get_pi_digits(n=NUM_MONS):
+def get_pi_digits(n=(NUM_MONS*3)+1):
 
   #First 2 characters are "3.", and the remaining digits are n-1
   #so we need 2 + (n-1) = n+1
@@ -21,13 +21,13 @@ pi_digits = get_pi_digits()
 
 
 def get_nth_pi_string(n=1):
-  pi_digits = get_pi_digits(NUM_MONS + 1)
+  pi_digits = get_pi_digits()
   starting_index = (n - 1) * 3
   return pi_digits[starting_index:starting_index + 3]
 
 
 def get_next_strings(start_string=1, num_strings=10):
-  pi_digits = get_pi_digits(NUM_MONS + 1)
+  pi_digits = get_pi_digits()
   for i in range(start_string, start_string + num_strings + 1):
     starting_digit = (i - 1) * 3
     print(i, pi_digits[starting_digit:starting_digit + 3])
